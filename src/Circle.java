e241b85fda648fa2c1515810cfbba71e574ac9d7
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-03-12 07:48:07 +0000

    Added 3 top buttons to dashboard

--- src/Circle.java
+++ src/Circle.java
@@ -4,16 +4,26 @@ public class Circle extends PApplet {
 
     PApplet pApplet;
     private float x, y, extent;
+    float r, g, b;
 
     Circle(PApplet pApplet, float x, float y, float extent){
         this.pApplet = pApplet;
         this.x = x;
         this.y = y;
         this.extent = extent;
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+    }
+
+    public void setColor(float red, float green, float blue){
+        this.r = red;
+        this.g = green;
+        this.b = blue;
     }
 
     public void drawCircle(){
-        pApplet.fill(0);
+        pApplet.fill(r, g, b);
         pApplet.noStroke();
         pApplet.smooth();
         pApplet.circle(x, y, extent);
--- src/Galaxy.java
+++ src/Galaxy.java
@@ -11,10 +11,32 @@ public class Galaxy extends PApplet {
 
         Arc arc1 = new Arc(this, 400, 350, 200, 200, radians(180), radians(360));
         arc1.drawArc();
-        Arc arc2 = new Arc(this, 550, 380, 160, 160, radians(210), radians(390));
-        arc2.drawArc();
-        Arc arc3 = new Arc(this,250, 380, 160, 160, radians(150), radians(330));
-        arc3.drawArc();
+        Arc rightArc = new Arc(this, 550, 380, 160, 160, radians(210), radians(390));
+        rightArc.drawArc();
+        Arc leftArc = new Arc(this,250, 380, 160, 160, radians(150), radians(330));
+        leftArc.drawArc();
+
+        Circle leftDashBoardCircle = new Circle(this, 550, 380, 150);
+        leftDashBoardCircle.drawCircle();
+        Circle rightDashBoardCircle = new Circle(this, 250, 380, 150);
+        rightDashBoardCircle.drawCircle();
+        Circle centreDashBoardCircle = new Circle(this, 400, 350, 180);
+        centreDashBoardCircle.drawCircle();
+
+        fill(16, 17, 21);
+        rect(300, 300, 200, 200);
+
+        Circle centreButton = new Circle(this, 400, 280, 20);
+        centreButton.setColor(81, 9, 1);
+        centreButton.drawCircle();
+
+        Circle leftButton = new Circle(this, 360, 287, 20);
+        leftButton.setColor(81, 9, 1);
+        leftButton.drawCircle();
+
+        Circle rightButton = new Circle(this, 440, 287, 20);
+        rightButton.setColor(81, 9, 1);
+        rightButton.drawCircle();
 
     }
 
