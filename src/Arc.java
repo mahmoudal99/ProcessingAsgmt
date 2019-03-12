e1e6fb7397d4655cf4406b5663e6c2fd667ec2d0
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-03-12 15:42:43 +0000

    Added Dashboard class

--- src/Arc.java
+++ src/Arc.java
@@ -4,6 +4,8 @@ public class Arc extends PApplet {
 
     PApplet pApplet;
     private float x, y, width, height, start, stop;
+    float r, g, b, strokeR, strokeG, strokeB;
+    public boolean noFill;
 
     Arc(PApplet pApplet, float x, float y, float width, float height, float start, float stop){
         this.pApplet = pApplet;
@@ -13,10 +15,40 @@ public class Arc extends PApplet {
         this.stop = stop;
         this.width = width;
         this.height = height;
+
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+
+        this.strokeR = 0;
+        this.strokeG = 0;
+        this.strokeB = 0;
+
+        this.noFill = false;
+
     }
 
+    public void setColor(float red, float green, float blue){
+        this.r = red;
+        this.g = green;
+        this.b = blue;
+    }
+
+    public void setStrokeColor(float red, float green, float blue){
+        this.strokeR = red;
+        this.strokeG = green;
+        this.strokeB = blue;
+    }
+
+
     public void drawArc(){
-        pApplet.fill(37,40,55);
+
+        if(noFill){
+            pApplet.noFill();
+        }else {
+            pApplet.fill(r, g, b);
+        }
+        pApplet.stroke(strokeR,strokeG,strokeB);
         pApplet.arc(x, y, width, height, start, stop);
     }
 }
---
+++ src/Dashboard.java
@@ -0,0 +1,84 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Dashboard extends PApplet {
+
+    PApplet pApplet;
+    PImage spaceShipImage;
+
+    public Dashboard(PApplet pApplet){
+
+        this.pApplet = pApplet;
+
+
+
+    }
+
+    public void drawDashboard(){
+        Arc arc1 = new Arc(pApplet, 400, 350, 200, 200, radians(180), radians(360));
+        arc1.setColor(37,40,55);
+        arc1.drawArc();
+        Arc rightArc = new Arc(pApplet, 550, 380, 160, 160, radians(210), radians(390));
+        rightArc.setColor(37,40,55);
+        rightArc.drawArc();
+        Arc leftArc = new Arc(pApplet,250, 380, 160, 160, radians(150), radians(330));
+        leftArc.setColor(37,40,55);
+        leftArc.drawArc();
+
+        Circle leftDashBoardCircle = new Circle(pApplet, 550, 380, 150);
+        leftDashBoardCircle.drawCircle();
+        Circle rightDashBoardCircle = new Circle(pApplet, 250, 380, 150);
+        rightDashBoardCircle.drawCircle();
+        Circle centreDashBoardCircle = new Circle(pApplet, 400, 350, 180);
+        centreDashBoardCircle.drawCircle();
+
+        pApplet.fill(16, 17, 21);
+        pApplet.rect(300, 300, 200, 200);
+
+        Circle centreButton = new Circle(pApplet, 400, 280, 20);
+        centreButton.setColor(81, 9, 1);
+        centreButton.drawCircle();
+
+        Circle leftButton = new Circle(pApplet, 360, 287, 20);
+        leftButton.setColor(81, 9, 1);
+        leftButton.drawCircle();
+
+        Circle rightButton = new Circle(pApplet, 440, 287, 20);
+        rightButton.setColor(81, 9, 1);
+        rightButton.drawCircle();
+
+        spaceShipImage = pApplet.loadImage("images/spaceship.png");
+        pApplet.image(spaceShipImage,210, 350, 64, 64);
+
+        Arc borderlineArc = new Arc(pApplet,240, 380, 100, 100, radians(180), radians(440));
+        borderlineArc.noFill = true;
+        borderlineArc.setStrokeColor(81, 9, 1);
+        borderlineArc.drawArc();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
