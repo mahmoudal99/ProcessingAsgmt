d2607abc46d0c6129fdf39c91d302cb45ee46f93
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-04-25 21:03:59 +0100

    Changed Planet Screen position

--- src/PlanetScreen.java
+++ src/PlanetScreen.java
@@ -4,33 +4,27 @@ public class PlanetScreen extends PApplet {
 
     PApplet pApplet;
 
-    PlanetScreen(PApplet pApplet){
+    PlanetScreen(PApplet pApplet) {
         this.pApplet = pApplet;
     }
 
-    void drawPlanetScren(String[] planets){
+    void drawPlanetScren(String[] planets) {
 
         pApplet.fill(255, 255, 255, 80);
         pApplet.stroke(200, 0, 0);
         pApplet.rect(650, 250, 120, 140, 7);
 
-//        int xLine = 140;
         int yLine = 260;
-//        while (xLine <= 200){
-//            pApplet.line(xLine, 450, xLine, 550);
-//            xLine += 10;
-//        }
-
         int planetNameCounter = 0;
 
-        while (yLine < 380){
-            pApplet.line(650, yLine+10, 770, yLine+10);
+        while (yLine < 380) {
+            pApplet.line(650, yLine + 10, 770, yLine + 10);
             pApplet.textSize(13);
             pApplet.fill(10, 255, 0);
-            pApplet.text(planets[planetNameCounter], 670, yLine+5);
+            pApplet.text(planets[planetNameCounter], 670, yLine + 5);
             yLine += 30;
 
-            if(planetNameCounter >= 4){
+            if (planetNameCounter >= 4) {
                 planetNameCounter = 0;
             }
             planetNameCounter++;
