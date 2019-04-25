d0c8b0c744b0c2b89413a1bc8984956fbab65d99
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-04-25 18:58:16 +0100

    Added Turrets class

--- src/Spaceship.java
+++ src/Spaceship.java
@@ -10,40 +10,8 @@ public class Spaceship extends PApplet {
 
     void drawRect() {
 
-        // Turrets
-        pApplet.strokeWeight(3);
-        pApplet.stroke(0, 0, 0);
-        pApplet.beginShape();
-        pApplet.vertex(70, 310);
-        pApplet.vertex(115, 330);
-        pApplet.vertex(75, 430);
-        pApplet.vertex(20, 400);
-        pApplet.endShape(CLOSE);
-
-        pApplet.beginShape();
-        pApplet.vertex(125, 275);
-        pApplet.vertex(145, 280);
-        pApplet.vertex(100, 324);
-        pApplet.vertex(85, 317);
-        pApplet.endShape(CLOSE);
-
-        pApplet.beginShape();
-        pApplet.vertex(730, 310);
-        pApplet.vertex(685, 330);
-        pApplet.vertex(725, 430);
-        pApplet.vertex(780, 400);
-        pApplet.endShape(CLOSE);
-
-        pApplet.beginShape();
-        pApplet.vertex(675, 275);
-        pApplet.vertex(655, 280);
-        pApplet.vertex(700, 324);
-        pApplet.vertex(715, 317);
-        pApplet.endShape(CLOSE);
-
-        pApplet.noStroke();
-        pApplet.fill(0);
-        pApplet.rect(0, 350, 800, 250);
+        Turrets turrets = new Turrets(pApplet);
+        turrets.drawTurrets();
 
         Windshield windshield = new Windshield(pApplet);
         windshield.drawWindshield();
---
+++ src/Turrets.java
@@ -0,0 +1,46 @@
+import processing.core.PApplet;
+
+public class Turrets extends Spaceship {
+
+    Turrets(PApplet pApplet) {
+        super(pApplet);
+    }
+
+    void drawTurrets(){
+
+        // Turrets
+        pApplet.strokeWeight(3);
+        pApplet.stroke(0, 0, 0);
+        pApplet.beginShape();
+        pApplet.vertex(70, 310);
+        pApplet.vertex(115, 330);
+        pApplet.vertex(75, 430);
+        pApplet.vertex(20, 400);
+        pApplet.endShape(CLOSE);
+
+        pApplet.beginShape();
+        pApplet.vertex(125, 275);
+        pApplet.vertex(145, 280);
+        pApplet.vertex(100, 324);
+        pApplet.vertex(85, 317);
+        pApplet.endShape(CLOSE);
+
+        pApplet.beginShape();
+        pApplet.vertex(730, 310);
+        pApplet.vertex(685, 330);
+        pApplet.vertex(725, 430);
+        pApplet.vertex(780, 400);
+        pApplet.endShape(CLOSE);
+
+        pApplet.beginShape();
+        pApplet.vertex(675, 275);
+        pApplet.vertex(655, 280);
+        pApplet.vertex(700, 324);
+        pApplet.vertex(715, 317);
+        pApplet.endShape(CLOSE);
+
+        pApplet.noStroke();
+        pApplet.fill(0);
+        pApplet.rect(0, 350, 800, 250);
+    }
+}
